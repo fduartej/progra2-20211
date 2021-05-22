a5675c83022029de53c63b104ed64b1df2ccc4ab
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-05-22 17:28:01 -0500

    Datos de Pago

---
+++ demomvc/src/main/java/edu/usmp/demomvc/dto/Pago.java
@@ -0,0 +1,16 @@
+package edu.usmp.demomvc.dto;
+
+import java.math.BigDecimal;
+
+public class Pago {
+
+    private String comercio;
+    private String numeroTarjeta;
+    private String nombreTitutal;
+    private String cvv;
+    private String fechaVencimiento;
+    private String tipoDocumento;
+    private String numeroDocumento;
+    private BigDecimal importe;
+    
+}
