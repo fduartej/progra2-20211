8bf8c9f55d77240361627e9eb810402a59bad67c
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-05-29 23:26:58 -0500

    Fix Post Dashboard

--- demodashboard/src/main/java/edu/usmp/demodashboard/controller/DashboardController.java
+++ demodashboard/src/main/java/edu/usmp/demodashboard/controller/DashboardController.java
@@ -7,11 +7,11 @@ import org.springframework.ui.Model;
 
 
 @Controller
-public class HomeController {
-    private static final String HOME_INDEX ="welcome"; 
+public class DashboardController {
+    private static final String PAYMENT_INDEX ="dashboard/payments"; 
 
-    @GetMapping("/welcome")
+    @GetMapping("/dashboard/payments")
     public String index(Model model) {
-        return HOME_INDEX;
+        return PAYMENT_INDEX;
     }
 }
